39cc1bfe2f7e8c7041c43ce87b9949e6a5f0526e
ABDUGANIYEV-01 <96335838+ABDUGANIYEV-01@users.noreply.github.com>
2022-06-23 10:43:22 +0500

    controller commit

---
+++ app/Http/Controllers/showProfileController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class showProfileController extends Controller
+{
+public function __invake( $id)
+    {
+        return 'Bir metodli Controller'.$id;
+    }
+}
